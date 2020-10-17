e7e0d586aca8cd8e91234be31126d43b1f985632
arifchuuw <achu123@uw.edu>
2020-10-18 03:03:39 +0800

    Multiplayer Server

---
+++ Icebreaking Game/Assets/Scripts/PhotonManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+//give us every callback unity has including monobehaviour
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("attempt connection");
+    }
+
+    //After conntecting to master, join a lobby.
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+        Debug.Log("connected to master");
+    }
+
+    //Create a room
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 4 }, TypedLobby.Default);
+    }
+
+    //Spawn in players
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("Player", new Vector2(Random.Range(-5f, 5f), transform.position.y), Quaternion.identity);
+    }
+}
---
+++ Icebreaking Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    bool jump;
+
+    private float x;
+    private Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if you press left it's x = -1, and right x = 1 of the axis
+        x = Input.GetAxis("Horizontal");
+        transform.position += (Vector3)new Vector2(x * speed * Time.deltaTime, 0);
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+    }
+
+    //Physics calculations updated
+    /*private void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }*/
+
+
+    void FixedUpdate()
+    {
+        if (jump)
+        {
+            rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+            jump = false;
+        }
+    }
+}
